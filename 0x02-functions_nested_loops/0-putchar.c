61acd6d3aa98ee311f795ed55a2b2d58de264f79
Mugisha Abdoul Latif <85370865+MugAbdoul@users.noreply.github.com>
2022-09-13 20:44:03 +0200

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,10 +1,13 @@
-#include "holberton.h"
+#include <unistd.h>
+
 /**
- * main - prints _putchar
- * Return: 0 on success.
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
  */
-int main(void)
+int _putchar(char c)
 {
-	printf("_putchar\n");
-	return (0);
+  return (write(1, &c, 1));
 }

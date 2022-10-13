769f78e05fed38dc1b10467a0eff0f5b3b5c9077
Mugisha Abdoul Latif <85370865+MugAbdoul@users.noreply.github.com>
2022-10-13 15:12:23 +0200

    Update 2-print_strings.c

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -1,41 +1,33 @@
-#include <stdio.h>
 #include "variadic_functions.h"
+#include <stdarg.h>
+#include <stdlib.h>
+#include <stdio.h>
+
 /**
- * print_strings - function that prints strings, followed by a new line.
- * @separator: input
- * @n: input
- * Return: output
+ * print_strings - prints all strings with a separator
+ * @separator: separe strings
+ * @n: int, number of undefined arguments
  */
+
 void print_strings(const char *separator, const unsigned int n, ...)
 {
-	va_list al;
-	unsigned int i;
-	char *value;
+va_list list;
+unsigned int i;
+char *c;
+va_start(list, n);
 
-	va_start(al, n);
-	for (i = 0; i < n; i++)
-	{
-		value = va_arg(al, char*);
-		if (value == 0)
-		{
-			printf("(nil)");
-		}
-		if (i == n - 1)
-		{
-			printf("%s", value);
-		}
-		if (i < n - 1)
-		{
-			if (separator != NULL)
-			{
-				printf("%s%s", value, separator);
-			}
-			else
-			{
-				printf("%s", value);
-			}
-		}
-	}
-	va_end(al);
-	printf("\n");
+for (i = 0; i < n; i++)
+{
+	c = va_arg(list, char*);
+	if (c != NULL)
+		printf("%s", c);
+	else
+		printf("(nil)");
+
+	if (i != n - 1 && separator != NULL)
+		printf("%s", separator);
+}
+va_end(list);
+
+putchar('\n');
 }
